cec5c89eca9ad3e1cd947d6ca8c4482859ceea16
Jérôme OGER <jerome.oger@sfr.fr>
2023-06-06 15:44:02 +0200

    Création du contenu de la page index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.scss';
+import { Row, Col, Card } from 'antd';
+import { FaPizzaSlice } from "react-icons/fa";
+import { GiFlamer } from "react-icons/gi";
+import { HiOutlineTruck } from "react-icons/hi";
+import { MdEuroSymbol } from "react-icons/md";
 
-function App() {
+
+const { Meta } = Card;
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="container">
+      <header>
+        <FaPizzaSlice />
+        <h1>Pizza 3000</h1>
       </header>
+
+      <div className="wrapper">
+
+        <Row gutter={20}>
+          <Col className="item" md={8}>
+            <Card
+              cover={<HiOutlineTruck />} >
+              <Meta title="Nouvelle Commande" description="Créer et enregistrer une nouvelle commande" />
+            </Card>
+          </Col>
+
+          <Col className="item" md={8}>
+            <Card
+              cover={<GiFlamer />} >
+              <Meta title="Commandes en cours" description="Voir le détail des commandes en cours" />
+            </Card>
+          </Col>
+
+          <Col className="item" md={8}>
+            <Card
+              cover={<MdEuroSymbol />} >
+              <Meta title="Paiement Commande" description="Encaisser une commande" />
+            </Card>
+          </Col>
+        </Row>
+
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
